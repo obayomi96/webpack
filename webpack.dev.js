3d7425c7cc630b3b55e281a2efc1890a5dfe6aea
martins obayomi <obayomimartins96@gmail.com>
2020-02-12 19:28:58 +0100

    Allow multiple entry points for external libraries or code

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,8 @@ const merge = require('webpack-merge');
 module.exports = merge(common, { // merge common webpack file with dev
   mode: 'development',
   output: {
-    filename: 'main.js',
+    filename: '[name].js', 
+    // To specify whatever name of the bundle file is when there is multiple
     path: path.resolve(__dirname, 'dist')
   },
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,9 @@ const common = require('./webpack.common');
 module.exports = merge(common, { // merge common webpack file with prod
   mode: 'production',
   output: {
-    filename: 'main.[contentHash].js', // use [contentHash] for cache busting
+    filename: '[name].[contentHash].bundle.js', 
+    // Use name to specify other bundle files aside main app bundle file
+    // use [contentHash] for cache busting
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [new CleanWebpackPlugin()] // To remove unused build files from dist
