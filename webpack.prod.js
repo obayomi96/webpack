6723269c3f1e91d92478cfd5ba1ee276e26077cc
martins obayomi <obayomimartins96@gmail.com>
2020-02-12 17:36:39 +0100

    File loaders and clean plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Must be destructured
 const common = require('./webpack.common');
 
-module.exports = merge(common, {
+module.exports = merge(common, { // merge common webpack file with prod
   mode: 'production',
   output: {
     filename: 'main.[contentHash].js', // use [contentHash] for cache busting
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [new CleanWebpackPlugin()] // To remove unused build files from dist
 });
